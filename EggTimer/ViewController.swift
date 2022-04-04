c50c3a90d4b93f89befa34ccf556ded5a8e0ca0d
Nyade <niinyade@icloud.com>
2022-04-04 19:32:14 -0300

    Added a progress bar and code for it. Also added a sound to play at the end, when the egg hardness selected is ready.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,37 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
        
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-    
-    var secondsRemaining = 60
-    
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer?
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
+                
         timer.invalidate()
         
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
         } else {
             timer.invalidate()
             titleLabel.text = "It's Ready!"
+            
+            func playSound() {
+                    let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                    player = try! AVAudioPlayer(contentsOf: url!)
+                player!.play()
+
+                }
+            }
         }
 
-}
-    
 }
