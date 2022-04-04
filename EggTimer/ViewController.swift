906aef726d5319c4eb82003fd1c032a2906bd12c
Nyade <niinyade@icloud.com>
2022-04-04 18:20:44 -0300

    Added the correct time in seconds for each option, a countdown function to the timer, as well as an invalidation for it to stop and also a label text change when the countdown reaches 0.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+       
+    @IBOutlet weak var titleLabel: UILabel!
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
         
-        let result = eggTimes[hardness]!
-            print(result)
-    }
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "It's Ready!"
+        }
+
+}
     
 }
